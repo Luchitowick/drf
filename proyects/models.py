9bb18320e13fec010b5e3962eaab75cde1c73850
Luchitowick <luisbecerra1111@gmail.com>
2024-12-05 11:18:52 -0300

    ready for render.com

--- proyects/models.py
+++ proyects/models.py
@@ -5,6 +5,3 @@ class Proyect(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
-
-
-# Create your models here.
